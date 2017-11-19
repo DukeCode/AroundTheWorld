b6fd8f478be9feb41f6d88dda1f391abb9137fd9
DukeCode <jid17@pitt.edu>
2017-11-19 18:14:10 -0500

    compile with GCE

--- service/main.go
+++ service/main.go
@@ -22,7 +22,7 @@ const (
 	PROJECT_ID = "aroundtheworld-185822"
 	BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.196.174.152:9200"
+	ES_URL = "http://35.196.118.54:9200"
 )
 
 
